be8437835379fb6f3fd015041464e6c24e804910
qingbing <780042175@qq.com>
2018-11-11 11:31:08 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# php-db-model
-db的ar操作
+# php-model
+## 描述
+db-model 的相关操作，定义
+
+## 注意事项
+ - model 的相关操作,包含有attributeNames、setAttributes、getAttributes、getAttributeLabel、getAttributeLabels 等
+ - 对于设置了 model 属性后，采用 validate 进行验
+
+## 使用方法
+```
+```
+## ====== 异常代码集合 ======
+
+异常代码格式：1012 - XXX - XX （组件编号 - 文件编号 - 代码内异常）
+```
+ - 101100101 : 在Mode
+```
---
+++ test/src/TestDbModel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Link         :   http://www.phpcorner.net
+ * User         :   qingbing<780042175@qq.com>
+ * Date         :   2018-11-11
+ * Version      :   1.0
+ */
+
+namespace Test;
+
+use TestCore\Tester;
+
+class TestDbModel extends Tester
+{
+    /**
+     * 执行函数
+     * @return mixed|void
+     * @throws \ReflectionException
+     * @throws \Exception
+     */
+    public function run()
+    {
+    }
+}
